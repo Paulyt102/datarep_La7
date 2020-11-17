5e11f1fe7cf5638036921913313e48e1dad8ed99
unknown <g00350075@gmit.ie>
2020-11-17 20:44:35 +0000

    Lab Done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 
@@ -21,51 +22,90 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+const myConnectionString ="mongodb+srv://admin:admin@cluster0.8v8zb.mongodb.net/movies?retryWrites=true&w=majority";
+mongoose.connect(myConnectionString, {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    title: String,
+year: String,
+poster: String
+});
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/api/movies', (req, res) => {
-    const movies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        },
-        {
-            "Title": "War of the Worlds",
-            "Year": "2005",
-            "imdbID": "tt0407304",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDUyODAzNDI1Nl5BMl5BanBnXkFtZTcwMDA2NDAzMw@@._V1_SX300.jpg"
-        }
-    ]
-    res.json({
-        mymovies: movies
-    })
-})
+var MovieModel = mongoose.model('movie', movieSchema);
 
+
+    
+app.get('/api/movies', (req, res) => {
+    // const movies = [
+    //     {
+    //         "Title": "Avengers: Infinity War",
+    //         "Year": "2018",
+    //         "imdbID": "tt4154756",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "Captain America: Civil War",
+    //         "Year": "2016",
+    //         "imdbID": "tt3498820",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "World War Z",
+    //         "Year": "2013",
+    //         "imdbID": "tt0816711",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "War of the Worlds",
+    //         "Year": "2005",
+    //         "imdbID": "tt0407304",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BNDUyODAzNDI1Nl5BMl5BanBnXkFtZTcwMDA2NDAzMw@@._V1_SX300.jpg"
+    //     }
+    // ]
+    // res.json({
+    //     mymovies: movies
+    // })
+    app.get('/api/movies/:id', (req, res, next)=> {
+        MovieModel.find(function (err, data) {
+        console.log(data);
+        res.json(data);
+        });
+        })
+        
+MovieModel.find((err, data)=>{
+    res.json(data);
+    });
+    
+    
 app.post('/api/movies', (req, res) => {
     console.log(req.body);
     res.send('Data Recieved!');
+    console.log(req.body.title);
+console.log(req.body.year);
+console.log(req.body.poster);
+
+MovieModel.create({
+title: req.body.title,
+year: req.body.year,
+poster: req.body.poster
+
+
 })
+
+res.send('item added');
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+})
 })
